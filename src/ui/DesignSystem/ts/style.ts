52f1872af9f550c99129f0dfe6dd062bee7ba6d5
Joao Koritar <jakoritarleite@gmail.com>
2021-03-20 12:50:22 -0300

    ui: Remove current UI files to rework with the new Design System

--- src/ui/DesignSystem/ts/style.ts
+++
@@ -1,9 +0,0 @@
-// Shared types and functionality related to styling
-
-export type ButtonVariant =
-  | "primary"
-  | "secondary";
-
-export function ellipsed(x: string, length: number = 10): string {
-  return `${x.slice(0, length + 2)}...${x.slice(-length)}`;
-}
--- src/ui/ts/session.ts
+++
@@ -1,6 +0,0 @@
-import { Readable, derived, get } from "svelte/store";
-
-export enum Status {
-    NoSession = "NO_SESSION",
-    SealedSession = ""
-}
